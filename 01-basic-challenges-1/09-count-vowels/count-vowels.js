15caefd6b9215cafc257cf4a422cc3153f1a4f1f
Jeffrey Ballinger <jeff@Mac.localdomain>
2025-10-18 11:48:11 -0500

    basic 1 complete

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,37 @@
-function countVowels() {}
+function countVowels(str) {
+    let vowelsCount = 0;
+    // let strippedVowels = str.map(char => isVowel(char))
+    for (let i = 0; i < str.length; i++) {
+        if (isVowel(str[i])) {
+            vowelsCount++;
+        }  
+    }
+    return vowelsCount
+}
+
+//helper function to check if a char is vowel
+function isVowel(char) {
+    const vowels = 'aeiouAEIOU';
+    return vowels.includes(char);
+}
+
+// regex solution
+// function isVowel(char) {
+//     return /^[aeiou]$/i.test(char); // 'i' flag for case-insensitive matching
+// }
+
 
 module.exports = countVowels;
+
+/*
+Explanation of methods:
+includes() on a string:
+This method checks if a string contains a specified substring. By creating a string of 
+all vowels (both uppercase and lowercase), you can directly check if the input character is present.
+
+Regular Expression:
+The regex /^[aeiou]$/i matches a string that consists of exactly one character, which must 
+be 'a', 'e', 'i', 'o', or 'u' (case-insensitive due to the i flag). The test() method of a 
+regex returns true if a match is found.
+
+*/
--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,12 @@
-function removeDuplicates() {}
+function removeDuplicates(arr) {
+    let noDuplicates = [];
+
+    for (let i = 0; i< arr.length; i++) {
+        if (!noDuplicates.includes(arr[i])) {
+            noDuplicates.push(arr[i]);
+        }
+    }
+    return noDuplicates;
+}
 
 module.exports = removeDuplicates;
