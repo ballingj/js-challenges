4d8132d1227e4de068b7681069d81f343a71393f
Jeffrey Ballinger <jeff@Mac.localdomain>
2025-10-18 16:16:53 -0500

    basic2 intersection

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,11 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+    const intersectedArray = [];
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr2.includes(arr1[i]) && !intersectedArray.includes(arr1[i])) {
+            intersectedArray.push(arr1[i]);
+        }
+    }
+    return intersectedArray;
+}
 
 module.exports = arrayIntersection;
--- 02-basic-challenges-2/02-array-intersection/array-intersection.test.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.test.js
@@ -5,5 +5,6 @@ test('Finding array intersection', () => {
     3, 4, 5,
   ]);
   expect(arrayIntersection([10, 20, 30], [30, 40, 50])).toEqual([30]);
+  expect(arrayIntersection([10, 10, 20, 20, 30], [20, 20, 30, 40, 50])).toEqual([20, 30]);
   expect(arrayIntersection([1, 2, 3], [4, 5, 6])).toEqual([]);
 });
