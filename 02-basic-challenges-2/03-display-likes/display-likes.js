e33650f834f42147cdffa74d5076b69e0cad134f
Jeffrey Ballinger <jeff@Mac.localdomain>
2025-10-18 16:59:44 -0500

    basic2 display-likes

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,18 @@
-function displayLikes() {}
+function displayLikes(names) {
+    const nameCount = names.length;
+
+    if (nameCount > 3) {
+        return `${names[0]}, ${names[1]} and ${nameCount - 2} others like this`
+    } else if (nameCount === 3) {
+        return `${names[0]}, ${names[1]} and ${names[2]} like this`
+    } else if (nameCount === 2) {
+        return `${names[0]} and ${names[1]} like this`
+    } else if (nameCount == 1) {
+        return `${names[0]} likes this`
+    } else {
+        return 'no one likes this'
+    }
+
+}
 
 module.exports = displayLikes;
