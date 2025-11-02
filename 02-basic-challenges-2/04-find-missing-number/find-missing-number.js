3e816bf1307e29f31fb0b1715c480489a0c33bb1
Jeffrey Ballinger <jeff@Mac.localdomain>
2025-11-02 11:18:32 -0600

    basic2 find missing number

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,5 +1,8 @@
 function findMissingNumber(arr) {
-    
+    // If the array is empty or undefined, return undefined
+    if (!arr || arr.length === 0) {
+        return undefined;
+    }
     n = arr.length + 1
     const sumOfNumbers = n * (n + 1) / 2
     const sumOfArray = arr.reduce((acc, elem) => acc + elem)
